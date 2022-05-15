d42843a2af6881cb6fd36b5e8876601f2d0e2501
NXture <varunvaru27101999@gmail.com>
2022-05-15 22:28:27 +0530

    created road object

---
+++ scripts/road.js
@@ -0,0 +1,41 @@
+class Road {
+  constructor(x, width, lanceCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.lanceCount = lanceCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const infinity = 1000000;
+    this.top = -infinity;
+    this.bottom = infinity;
+  }
+
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.lanceCount;
+    return (
+      this.left +
+      laneWidth / 2 +
+      Math.min(laneIndex, this.lanceCount - 1) * laneWidth
+    );
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 0; i <= this.lanceCount; i++) {
+      const x = lerp(this.left, this.right, i / this.lanceCount);
+      if (i > 0 && i < this.lanceCount) {
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
+  }
+}
