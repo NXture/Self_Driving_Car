a2e302ea7f6b7d60d3f55abf283a61a308ff654a
NXture <varunvaru27101999@gmail.com>
2022-05-16 00:41:28 +0530

    added collision physics against dummy car

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,16 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50,"KEYS");
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)];
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].update(road.borders);
+    traffic[i].update(road.borders, []);
   }
-  car.update(road.borders);
+  car.update(road.borders, traffic);
 
   canvas.height = window.innerHeight;
 
@@ -20,8 +20,8 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.8);
   road.draw(ctx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx);
+    traffic[i].draw(ctx, "red");
   }
-  car.draw(ctx);
+  car.draw(ctx, "blue");
   requestAnimationFrame(animate);
 }
