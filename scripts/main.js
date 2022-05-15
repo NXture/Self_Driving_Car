620ea0d17a9186e5c2102e12eced5f8215ac5366
NXture <varunvaru27101999@gmail.com>
2022-05-15 22:38:43 +0530

    implemented top camera & collision border

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,9 @@ function animate() {
   car.update();
 
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.8);
   road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
